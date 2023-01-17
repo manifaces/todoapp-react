0cbfb16d72da4d97b2e56ad104459aecacda0952
manifaces.dev <manifaces.dev@gmail.com>
2023-01-17 13:21:27 +0300

    add hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import store from './store';
 import './index.scss';
@@ -9,9 +9,9 @@ import AppToDo from './AppToDo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <AppToDo />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
